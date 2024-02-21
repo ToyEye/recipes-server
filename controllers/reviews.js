885dbea3ed774a0a769478f5ee8758f183e764a7
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-02-21 17:05:15 +0200

    add ctrls

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,1 +1,16 @@
 import { ctrlWrapper } from "../controllers/countries.js";
+
+const getRecipeReviews = async (req, res) => {};
+
+const addReview = async (req, res) => {};
+
+const changeReviewById = async (req, res) => {};
+
+const deleteReviewById = async (req, res) => {};
+
+export default {
+  getRecipeReviews: ctrlWrapper(getRecipeReviews),
+  addReview: ctrlWrapper(addReview),
+  changeReviewById: ctrlWrapper(changeReviewById),
+  deleteReviewById: ctrlWrapper(deleteReviewById),
+};
