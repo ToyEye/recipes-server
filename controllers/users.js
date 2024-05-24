91c2e40d22162d7bee4c847ae3702f86f7cc1d95
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-24 13:27:13 +0300

    add getCurrentUser

--- controllers/users.js
+++ controllers/users.js
@@ -76,7 +76,7 @@ const getCurrentUser = async (req, res) => {
 
   const currentUser = await User.findById(_id);
 
-  return currentUser;
+  res.json({ user: currentUser.name, email: currentUser.email });
 };
 
 export default {
